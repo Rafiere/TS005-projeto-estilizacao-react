23c4878b46ab26f9d48920d49e781efee53619dd
Rafael <rafapgbr@gmail.com>
2022-08-31 20:45:46 -0300

    feat: criação do componente "Dashboard" básico.

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,8 @@
+import './Dashboard.css'; //O React utilizará, de forma automática, esse arquivo CSS.
+
+export const Dashboard = () => {
+
+    return(
+        <p className="paragraph">Teste</p>
+    );
+}
